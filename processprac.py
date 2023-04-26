68066053a2e74427f966ec682be2246cc9eb2a58
hanilcome <hanilcome@naver.com>
2023-04-26 10:18:52 +0900

    코루틴 예제 추가

--- processprac.py
+++ processprac.py
@@ -115,7 +115,7 @@ del my_dict["a"]
 print(my_dict)
 
 # 딕셔너리에서 특정 key에 해당하는 value값 얻기
-my_dict = {"a": 1, "b": 2, "c": 3}
+my_dict = {"a": '1', "b": 2, "c": 3}
 print(my_dict["a"])
 
 # 딕셔너리에서 모든 key를 리스트로 만들기
@@ -184,3 +184,75 @@ if __name__ == '__main__':
     thread1 = threading.Thread(target=foo).start()
     thread2 = threading.Thread(target=foo).start()
     thread3 = Process(target=foo).start()
+
+# 파이썬 코루틴 예제1
+def my_coroutine():
+    while True:
+        value = yield
+        print('Received value:', value)
+        
+co = my_coroutine()
+
+co.send('Hello, world!')
+
+# 파이썬 코루틴 예제2
+def my_generator():
+    yield 1
+    yield 2
+    yield 3
+
+gen = my_generator()
+print(next(gen)) # 1
+print(next(gen)) # 2
+print(next(gen)) # 3
+
+# 파이썬 코루틴 예제3
+def my_coroutine():
+    while True:
+        x = yield
+        print('Received:', x)
+
+co = my_coroutine()
+next(co)
+
+co.send(10) # Received: 10
+co.send(20) # Received: 20
+co.send(30) # Received: 30
+
+# 파이썬 코루틴 예제4
+phone_book = {"John":"123-4567", "Jane":"234-5678", "Max":"345-6789"}
+def search():
+    name = yield
+    while True:
+        if name in phone_book:
+            phone_number = phone_book[name]
+        else:
+            phone_number = "Cannot find the name in the phone book"
+        name = yield phone_number
+        
+search_coroutine = search()
+next(search_coroutine)
+
+result = search_coroutine.send("John")
+print(result)
+
+result = search_coroutine.send("Jane")
+print(result)
+
+result = search_coroutine.send("Sarah")
+print(result)
+
+# asyncio
+import asyncio
+import random
+
+async def fetch_data():
+    print("데이터를 가져오는 중...")
+    await asyncio.sleep(1) # 데이터를 가져오는데 1초가 걸린다고 가정
+    return random.randint(1, 100)
+
+async def main():
+    data = await fetch_data()
+    print(f"가져온 데이터: {data}")
+
+asyncio.run(main())
